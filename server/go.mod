1af3a0ffcd06efdd000cd984323aaab1a2bb011f
pg <pg830616@163.com>
2022-03-31 22:49:12 +0800

    读配置

--- server/go.mod
+++ server/go.mod
@@ -1,7 +1,17 @@
 module gitee.com/console/server
 
-go 1.16
+go 1.18
 
 require (
+	github.com/BurntSushi/toml v0.4.1
 	github.com/gobwas/ws v1.1.0
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/mattn/go-colorable v0.1.12
+)
+
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"gitee.com/console/server/util"
+	"github.com/BurntSushi/toml"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var (
+	config = &struct {
+		Server   string
+		Username string
+		Password string
+	}{"127.0.0.1:3306", "root", "123456"}
+	ConfigRaw []byte
+)
+
 func main() {
+	var err error
+	addr := flag.String("c", "config.toml", "config file")
+	flag.Parse()
+	if err := util.CreateShutdownScript(); err != nil {
+		util.Print("create shutdown script error:", err)
+	}
+
+	if ConfigRaw, err = ioutil.ReadFile(*addr); err != nil {
+		util.Print("read config file error:", err)
+		return
+	}
+
+	var cg map[string]interface{}
+
+	if _, err = toml.Decode(string(ConfigRaw), &cg); err == nil {
+		if cfg, ok := cg["Mysql"]; ok {
+			b, _ := json.Marshal(cfg)
+			if err = json.Unmarshal(b, config); err != nil {
+				log.Println(err)
+			}
+		}
+	} else {
+		util.Print("decode config file error:", err)
+	}
+
+	util.Print("server is ", config.Server)
+
 	fmt.Println("start server at 9999")
-	http.HandleFunc("/test",func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("start server at 9999")
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
