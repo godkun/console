0e7dfb4ce7bfa88a4a42cf4640895d9c1702f7f4
pg <pg830616@163.com>
2022-04-25 23:18:21 +0800

    写个上传

--- server/main.go
+++ server/main.go
@@ -12,8 +12,11 @@ import (
 	"github.com/gobwas/ws/wsutil"
 	"io/ioutil"
 	"log"
+	"mime"
 	_ "net"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 var MysqlDb *sql.DB
@@ -103,6 +106,8 @@ func init() {
 	}
 }
 
+const maxUploadSize = 2 * 1024 * 2014 // 2 MB
+const uploadPath = "./files"
 func main() {
 	//util.SendMailUsingTLS(config.SMTPserver, config.SMTPport, config.SMTPshowname, "pg830616@163.com",
 	//	"hello", config.SMTPpassword, config.SMTPusername, "注册验证码")
@@ -113,9 +118,13 @@ func main() {
 	util.Print("server is ", config.Server)
 
 	fmt.Println("start server at 9999")
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/api/user/register", register)
 	http.HandleFunc("/api/user/getverifycode", getVerifyCode)
 	http.HandleFunc("/api/user/login", login)
+	http.HandleFunc("/upload", uploadFileHandler())
+	fs := http.FileServer(http.Dir(uploadPath))
+	http.Handle("/files/", http.StripPrefix("/files", fs))
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("start server at 9999")
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
@@ -139,6 +148,77 @@ func main() {
 	})
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
+
+func uploadFileHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		CORS(w, r)
+		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+			fmt.Printf("Could not parse multipart form: %v\n", err)
+			renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
+			return
+		}
+
+		// parse and validate file and post parameters
+		file, fileHeader, err := r.FormFile("file")
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		// Get and print out file size
+		fileSize := fileHeader.Size
+		fmt.Printf("File size (bytes): %v\n", fileSize)
+		// validate file size
+		if fileSize > maxUploadSize {
+			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+			return
+		}
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			renderError(w, "INVALID_FILE", http.StatusBadRequest)
+			return
+		}
+
+		// check file type, detectcontenttype only needs the first 512 bytes
+		detectedFileType := http.DetectContentType(fileBytes)
+		switch detectedFileType {
+		case "image/jpeg", "image/jpg":
+		case "image/gif", "image/png":
+		case "application/pdf":
+			break
+		default:
+			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
+			return
+		}
+		fileName := util.RandNumStr(12)
+		fileEndings, err := mime.ExtensionsByType(detectedFileType)
+		if err != nil {
+			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
+			return
+		}
+		newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
+		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+
+		// write file
+		newFile, err := os.Create(newPath)
+		if err != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+		defer newFile.Close() // idempotent, okay to call twice
+		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
+			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+			return
+		}
+		w.Write(util.ErrJson(util.OK))
+	})
+}
+
+func renderError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(message))
+}
+
 
 /**
 登录
