9f6e7da1d88039f418b61219e17fae6d09fa11fc
godkun <godkun666@126.com>
2022-05-22 18:32:45 +0800

    feat: 增加流水线

---
+++ client/scripts/deploy.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env zx
+import { $ } from 'zx'
+import path from 'path'
+let dist = path.resolve(__dirname, '../', 'dist')
+await $`npm run build && scp -r ${dist} root@124.223.73.152:/usr/local/nginx/html/`
+
+// ssh 密码： Monibuca@4
