5e6ea0346eab657a8f395c2e85c902eadc1899f6
godkun <godkun666@126.com>
2022-07-05 22:06:02 +0800

    feat: 调整m7sid

--- client/src/api/instance/index.ts
+++ client/src/api/instance/index.ts
@@ -10,12 +10,6 @@ function queryURLparamsRegEs6(url) {
   return obj
 }
 
-const href = window.location.href
-const query = queryURLparamsRegEs6(href)
-let id  = query.id
-if (!id) id = localStorage.getItem('id')
-
-
 // 获取实例列表
 export function getInstanceList(data) {
   return fetch({
@@ -54,6 +48,10 @@ export function delInstance(data) {
 
 // 获取采样数据,包括 CPU、内存、网卡数据、以及流信息
 export function getInstanceSummary() {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: '/summary',
     method: 'post',
@@ -65,6 +63,10 @@ export function getInstanceSummary() {
 
 // 系统信息，包含版本号（Version）和启动时间（StartTime）两个字段
 export function getSysInfo() {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: '/sysinfo',
     method: 'post',
@@ -76,6 +78,10 @@ export function getSysInfo() {
 
 // 获取流（live/test）的详细信息
 export function getStreamDetail(streamPath) {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: `/stream?streamPath=${streamPath}`,
     method: 'post',
@@ -87,6 +93,10 @@ export function getStreamDetail(streamPath) {
 
 // 停止流
 export function stopStream(params) {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: '/stopstream',
     method: 'post',
@@ -99,6 +109,10 @@ export function stopStream(params) {
 
 // 获取配置文件信息，可以加参数 name=xxx，获取 xxx 插件的配置信息（不加参数则获取全局配置信息）
 export function getConfig(name) {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: name ? `/getconfig?name=${name}` : `/getconfig`,
     method: 'post',
@@ -120,6 +134,10 @@ export function updateConfig(data) {
 // 修改配置信息，可以加参数 name=xxx，代表修改 xxx 插件的配置信息（不加参数则修改全局配置信息）
 // 修改的配置信息通过请求的 body（JSON 格式）提交
 export function modifyConfig(data, name) {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: name ? `/modifyconfig?name=${name}` : `/modifyconfig`,
     method: 'post',
@@ -132,6 +150,10 @@ export function modifyConfig(data, name) {
 
 // 获取实例所有插件
 export function getInstancePlugin(params) {
+  const href = window.location.href
+  const query = queryURLparamsRegEs6(href)
+  let id = query.id
+  if (!id) id = localStorage.getItem('id')
   return fetch({
     url: '/plugins',
     method: 'post',
