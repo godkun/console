d121dc6fbe5843c58425ee05e6112a384ef4ad0b
dexter <178529795@qq.com>
2022-08-14 19:36:46 +0800

    显示通道列表

--- client/src/views/instance/detail/gb28181/columns.ts
+++ client/src/views/instance/detail/gb28181/columns.ts
@@ -1,5 +1,43 @@
-import { h } from 'vue'
-import {NTime } from 'naive-ui'
+import { h } from 'vue';
+import { NTime } from 'naive-ui';
+import { BasicTable } from '@/components/Table';
+const channelsColumns = [
+  {
+    title: 'ID',
+    key: 'DeviceID',
+    width: 100
+  },
+  {
+    title: 'Name',
+    key: 'Name',
+    width: 70
+  },
+  {
+    title: 'Status',
+    key: 'Status',
+    width: 70
+  },
+  {
+    title: 'Manufacturer',
+    key: 'Manufacturer',
+    width: 100
+  },
+  {
+    title: 'Model',
+    key: 'Model',
+    width: 50
+  },
+  {
+    title: 'Owner',
+    key: 'Owner',
+    width: 70
+  },
+  {
+    title: 'Address',
+    key: 'Address',
+    width: 120
+  },
+];
 export const columns = [
   {
     title: 'ID',
@@ -9,6 +47,11 @@ export const columns = [
   {
     title: 'Name',
     key: 'Name',
+    width: 70
+  },
+  {
+    title: 'Status',
+    key: 'Status',
     width: 70
   },
   {
@@ -31,7 +74,7 @@ export const columns = [
     key: 'RegisterTime',
     width: 130,
     render(row) {
-      return h(NTime, row.RegisterTime)
+      return h(NTime, row.RegisterTime);
     }
   },
   {
@@ -39,25 +82,28 @@ export const columns = [
     key: 'UpdateTIme',
     width: 130,
     render(row) {
-      return h(NTime, row.UpdateTIme)
+      return h(NTime, row.UpdateTIme);
     }
   },
-  {
-    title: 'Status',
-    key: 'Status',
-    width: 70
-  },
+
   {
     title: 'NetAddr',
     key: 'NetAddr',
     width: 120
   },
   {
+    type: 'expand',
+    expandable: (rowData) => rowData.Channels.length > 0,
+    renderExpand: (rowData) => {
+      return h(BasicTable, { pagination: false, dataSource: rowData.Channels, columns: channelsColumns });
+    }
+  },
+  {
     title: 'Channels',
     key: 'Channels',
     width: 70,
     render(row) {
-      return h('text', row.Channels.length)
+      return h('text', row.Channels.length);
     }
   }
-]
+];
