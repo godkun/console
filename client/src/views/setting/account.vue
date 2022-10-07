77fe07abfb8ddbabb14adb91ffd9f5f460f31d49
godkun <godkun666@126.com>
2022-10-07 22:45:56 +0800

    fix: 优化设置页面UI

--- client/src/views/setting/account.vue
+++ client/src/views/setting/account.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <n-grid :x-gap="24">
-      <n-grid-item span="6">
-        <n-card :bordered="false" size="small" class="proCard">
+  <n-grid :x-gap="24">
+    <n-grid-item span="6">
+      <n-list>
+        <n-list-item>
           <n-thing
             class="thing-cell"
             v-for="item in typeTabList"
@@ -10,17 +10,17 @@
             :class="{ 'thing-cell-on': type === item.key }"
             @click="switchType(item)">
             <template #header>{{ item.name }}</template>
-            <template #description>{{ item.desc }}</template>
           </n-thing>
-        </n-card>
-      </n-grid-item>
-      <n-grid-item span="18">
-        <n-card :bordered="false" size="small" :title="typeTitle" class="proCard">
-          <BasicSetting v-if="type === 1" />
-        </n-card>
-      </n-grid-item>
-    </n-grid>
-  </div>
+        </n-list-item>
+      </n-list>
+    </n-grid-item>
+    <n-grid-item span="18">
+      <n-card :bordered="false" size="small" :title="typeTitle" class="proCard">
+        <BasicSetting v-if="type === 1" />
+        <PM2 v-if="type === 2" />
+      </n-card>
+    </n-grid-item>
+  </n-grid>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue'
@@ -42,25 +42,21 @@
 </script>
 <style lang="less" scoped>
   .thing-cell {
-    margin: 0 -16px 10px;
-    padding: 5px 16px;
+    margin: 12px;
+    margin-top: 0px;
+    padding: 15px;
 
     &:hover {
-      background: #f3f3f3;
+      background: #f0faff;
       cursor: pointer;
     }
   }
 
   .thing-cell-on {
     background: #f0faff;
-    color: #2d8cf0;
 
     ::v-deep(.n-thing-main .n-thing-header .n-thing-header__title) {
       color: #2d8cf0;
-    }
-
-    &:hover {
-      background: #f0faff;
     }
   }
 </style>
