d2fa9bc11740172f29f4109ead2f15e6d7a14c72
dexter <178529795@qq.com>
2022-10-11 20:41:26 +0800

    部署脚本采用域名

--- client/scripts/deploy-console.mjs
+++ client/scripts/deploy-console.mjs
@@ -2,6 +2,6 @@
 import { $ } from 'zx'
 import path from 'path'
 const dist = path.resolve(__dirname, '../', 'dist')
-await $`npm run build && scp -r ${dist} root@124.223.73.152:/usr/local/nginx/html/`
+await $`npm run build && scp -r ${dist} root@console.monibuca.com:/usr/local/nginx/html/`
 
 // ssh 密码： Monibuca@4
