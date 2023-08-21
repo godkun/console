65d41bcd1fd19c1441981605297fac213a55764b
godkun <godkun666@126.com>
2023-08-21 21:53:09 +0800

    feat: 修复点击页面没反应

--- client/src/wails.ts
+++ client/src/wails.ts
@@ -1,11 +1,11 @@
 import { setupApp } from './app';
-console.log = (...args: any[]) => {
-  window['go']['main']['App']['Greet'](args.map(String));
-};
-console.error = (...args: any[]) => {
-  window['go']['main']['App']['Greet'](args.map(String));
-};
-console.warn = (...args: any[]) => {
-  window['go']['main']['App']['Greet'](args.map(String));
-};
+// console.log = (...args: any[]) => {
+//   window['go']['main']['App']['Greet'](args.map(String));
+// };
+// console.error = (...args: any[]) => {
+//   window['go']['main']['App']['Greet'](args.map(String));
+// };
+// console.warn = (...args: any[]) => {
+//   window['go']['main']['App']['Greet'](args.map(String));
+// };
 setupApp();
